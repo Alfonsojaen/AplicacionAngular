4e45ee018aa46fa0a503577ec99b09e53c249d16
juanmariiaa <arizaserranojuanmaria@gmail.com>
2025-01-15 22:22:12 +0100

    Ultma implementacion de api

--- src/app/pages/recomendaciones/recomendaciones.component.ts
+++ src/app/pages/recomendaciones/recomendaciones.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TmdbService } from '../../services/tmdb.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-recomendaciones',
@@ -13,13 +13,32 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class RecomendacionesComponent implements OnInit {
   popularMovies: any[] = []; // Variable para almacenar las películas populares
+  topRatedMovies: any[] = []; // Variable para almacenar las películas mejor valoradas
+  searchResults: any[] = []; // Variable para almacenar los resultados de búsqueda
+  searchForm: FormGroup; // Formulario de búsqueda
 
-  constructor(private tmdbService: TmdbService) { // Inyecta el servicio TmdbService
+  constructor(private tmdbService: TmdbService, private fb: FormBuilder) { // Inyecta el servicio TmdbService y FormBuilder
+    this.searchForm = this.fb.group({
+      query: ['']
+    });
   }
 
   ngOnInit() {
     this.tmdbService.getPopularMovies().subscribe(movies => {
       this.popularMovies = movies.results; // Guarda las películas populares en la variable
     });
+
+    this.tmdbService.getTopRatedMovies().subscribe(movies => {
+      this.topRatedMovies = movies.results; // Guarda las películas mejor valoradas en la variable
+    });
+  }
+
+  onSearch() {
+    const query = this.searchForm.get('query')?.value;
+    if (query) {
+      this.tmdbService.searchMovies(query).subscribe(results => {
+        this.searchResults = results.results; // Guarda los resultados de búsqueda en la variable
+      });
+    }
   }
 }
--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -8,6 +8,10 @@ import { Observable } from 'rxjs';
 export class TmdbService implements OnInit {
   private apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZTBkYWU0MGVhYTlkYmQyOTJhYThlZjAyMjZiNjNkZiIsIm5iZiI6MTczMzkwOTcyMi4yMTEsInN1YiI6IjY3NTk1Y2RhNTA2YjQyMzk0ZDIxNTZhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.83ZGFvexWh_hdfPjvwP54KFZdZDmucWhIj49Ho3KZGA';
   private apiUrl = 'https://api.themoviedb.org/3/movie/popular';
+  private searchUrl = 'https://api.themoviedb.org/3/search/movie';
+  private topRatedUrl = 'https://api.themoviedb.org/3/movie/top_rated';
+
+
 
   constructor(private http: HttpClient) {}
 
@@ -20,5 +24,19 @@ export class TmdbService implements OnInit {
       'Authorization': `Bearer ${this.apiKey}`
     });
     return this.http.get<any>(this.apiUrl, { headers });
+  }
+
+  searchMovies(query: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.apiKey}`
+    });
+    return this.http.get<any>(`${this.searchUrl}?query=${query}`, { headers });
+  }
+
+  getTopRatedMovies(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.apiKey}`
+    });
+    return this.http.get<any>(this.topRatedUrl, { headers });
   }
 }
